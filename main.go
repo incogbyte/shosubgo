b811a4002f3745d903a33c5eec3f1841d4c0d7e7
inc0gbyt3 <53656948+incogbyte@users.noreply.github.com>
2020-02-07 14:56:41 -0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"./apishodan"
+)
+
+func main() {
+	domain := flag.String("d", "", "[+] Domain to find subdomains")
+	shodanKey := flag.String("s", "", "[+] Shodan api key")
+	verbose := flag.Bool("v", false, "[+] Show all output")
+	flag.Parse()
+
+	if *domain == "" || *shodanKey == "" {
+		fmt.Printf("Usage %s -d target.com -s MYShodaNKey", os.Args[0])
+		os.Exit(1)
+	}
+
+	apiKey := apishodan.New(*shodanKey)
+
+	info, err := apiKey.InfoAccount()
+	if err != nil {
+		log.Panicln(err)
+	}
+	fmt.Printf(
+		"Credits: %d\nScan Credits: %d \n\n",
+		info.QueryCredits, info.ScanCredits)
+
+	domainSearch := *domain
+
+	subdomain, err := apiKey.GetSubdomain(domainSearch)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	fmt.Printf("[+] Domain search: %s\n", domainSearch)
+
+	if *verbose == true {
+
+		for _, v := range subdomain.Data {
+			d := v.SubD + subdomain.Domain
+			fmt.Printf("Domain: %s\nIP/DNS :%s\nLast Scan made by shodan:%s\n", d, v.Value, v.LastSeen)
+		}
+
+	} else {
+		for _, v := range subdomain.SubDomains {
+			fmt.Printf("%s.%s\n", v, domainSearch)
+		}
+	}
+}
